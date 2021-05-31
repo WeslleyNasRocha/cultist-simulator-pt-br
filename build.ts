e11eda950cf89459daef3af7d3c0b4ba9d6d7919
Weslley Rocha <weslleytato.12@gmail.com>
2021-05-31 02:01:58 -0300

    update files

--- build.ts
+++
@@ -1,9 +0,0 @@
-import { readdir } from 'fs/promises';
-
-try {
-  const files = await readdir('translations');
-  for await (const file of files)
-    console.log(file);
-} catch (err) {
-  console.error(err);
-}
